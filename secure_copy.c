c95802e5486d62406c15215626194b1076ad5111
Ivan Yurasov <crazyurasov@gmail.com>
2026-03-06 18:08:15 +0300

    Очередь

--- secure_copy.c
+++ secure_copy.c
@@ -1,216 +1,236 @@
-#define _POSIX_C_SOURCE 200809L
-#include <pthread.h>
-#include <signal.h>
-#include <errno.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <signal.h>
 #include <string.h>
 #include <unistd.h>
+#include "caesar.h"
 
-#include "caesar.h"   // из задания 1
+#define BUFFER_SIZE 4096
 
-#ifndef BUF_SIZE
-#define BUF_SIZE 8192
-#endif
+typedef struct queue_node { //информация о структуре узла очереди
+    unsigned char* data;
+    int size;
+    struct queue_node* next;
+} queue_node;
 
-static volatile sig_atomic_t keep_running = 1;
+typedef struct { //информация о структуре очереди
+    queue_node* head;
+    queue_node* tail;
+    int count;
 
-static void on_sigint(int signo) {
-    (void)signo;
-    keep_running = 0;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond_empty;
+} queue;
+
+void queue_init(queue* q) { //инициализация очереди
+    q->head = NULL; //установка указателя на голову очереди в NULL
+    q->tail = NULL; //установка указателя на хвост очереди в NULL
+    q->count = 0; //установка счетчика элементов в очереди в 0
+    pthread_mutex_init(&q->mutex, NULL);  //инициализация мьютекса для синхронизации доступа к очереди
+    pthread_cond_init(&q->cond_empty, NULL); //мьютекс и условная переменная для синхронизации доступа к очереди
 }
 
-typedef struct {
-    FILE* in;
-    FILE* out;
+void queue_push(queue* q, unsigned char* data, int size) { //добавление элемента в очередь
+    queue_node* node = malloc(sizeof(queue_node));
+    node->data = malloc(size);
+    memcpy(node->data, data, size); //копирование данных в новый узел
+    node->size = size;
+    node->next = NULL;
 
-    unsigned char buf[BUF_SIZE];
-    size_t len;        
-    int has_data;      
-    int eof;           
+    pthread_mutex_lock(&q->mutex);  //блокировка мьютекса для синхронизации доступа к очереди
 
-    pthread_mutex_t m;
-    pthread_cond_t  can_produce;
-    pthread_cond_t  can_consume;
-} shared_t;
+    if (q->tail == NULL) {
+        q->head = q->tail = node;
+    } else {
+        q->tail->next = node;
+        q->tail = node;
+    }
+    q->count++;
 
-static void die(const char* msg) {
-    perror(msg);
+    pthread_cond_signal(&q->cond_empty);
+    pthread_mutex_unlock(&q->mutex);
+}
+
+unsigned char* queue_pop(queue* q, int* size) { //удаление элемента из очереди и возвращение его данных и размера
+    pthread_mutex_lock(&q->mutex);
+
+    while (q->head == NULL) {
+        pthread_cond_wait(&q->cond_empty, &q->mutex);
+    }
+
+    queue_node* node = q->head;
+    unsigned char* data = node->data;
+    *size = node->size;
+
+    q->head = node->next;
+    if (q->head == NULL) {
+        q->tail = NULL;
+    }
+    q->count--;
+
+    free(node);
+
+    pthread_mutex_unlock(&q->mutex);
+    return data;
+}
+
+void queue_destroy(queue* q) { //освобождение ресурсов, связанных с очередью
+    pthread_mutex_lock(&q->mutex);
+
+    queue_node* current = q->head;
+    while (current != NULL) {
+        queue_node* next = current->next;
+        free(current->data);
+        free(current);
+        current = next;
+    }
+
+    q->head = NULL;
+    q->tail = NULL;
+    q->count = 0;
+
+    pthread_mutex_unlock(&q->mutex);
+    pthread_mutex_destroy(&q->mutex);
+    pthread_cond_destroy(&q->cond_empty);
+}
+ //глобальные переменные для управления состоянием программы и хранения текущей очереди
+volatile int keep_running = 1;
+queue current_queue;
+volatile char* output_path_to_remove = NULL;
+
+typedef struct { //структура для хранения общих данных между потоками
+    FILE* input;
+    FILE* output;
+    queue q;
+    int finished;
+
+    pthread_mutex_t mutex;
+} shared_data;
+
+void handle_sigint(int sig) { //обработчик сигнала для прерывания программы
+    (void)sig;
+
+    keep_running = 0;
+
+    printf("\nОперация прервана пользователем\n");
+
+    queue_destroy(&current_queue);
+
+    if (output_path_to_remove != NULL) {
+        remove(output_path_to_remove);
+    }
+
     exit(1);
 }
 
-static int timed_wait_ms(pthread_cond_t* c, pthread_mutex_t* m, long ms) {
-    struct timespec ts;
-    if (clock_gettime(CLOCK_REALTIME, &ts) != 0) return -1;
+void* producer(void* arg) { //поток производителя, который читает данные из входного файла, шифрует их и помещает в очередь
 
-    long nsec = ts.tv_nsec + ms * 1000000L;
-    ts.tv_sec  += nsec / 1000000000L;
-    ts.tv_nsec  = nsec % 1000000000L;
+    shared_data* data = (shared_data*)arg;
 
-    int rc = pthread_cond_timedwait(c, m, &ts);
-    if (rc == 0 || rc == ETIMEDOUT) return rc;
-    return rc;
-}
-
-static void* producer_thread(void* arg) {
-    shared_t* sh = (shared_t*)arg;
+    unsigned char temp[BUFFER_SIZE];
 
     while (keep_running) {
-        pthread_mutex_lock(&sh->m);
 
-        while (sh->has_data && keep_running) {
-            timed_wait_ms(&sh->can_produce, &sh->m, 200);
-        }
+        int bytes = fread(temp, 1, BUFFER_SIZE, data->input);
 
-        if (!keep_running) {
-            pthread_mutex_unlock(&sh->m);
+        if (bytes <= 0) {
+            pthread_mutex_lock(&data->mutex);
+            data->finished = 1;
+            pthread_mutex_unlock(&data->mutex);
             break;
         }
 
-        pthread_mutex_unlock(&sh->m);
+        unsigned char* encrypted = malloc(bytes);
+        caesar(temp, encrypted, bytes);
 
-        size_t n = fread(sh->buf, 1, BUF_SIZE, sh->in);
-        if (n == 0) {
-            if (ferror(sh->in)) {
-                keep_running = 0;
-            }
-            pthread_mutex_lock(&sh->m);
-            sh->eof = 1;
-            pthread_cond_signal(&sh->can_consume);
-            pthread_mutex_unlock(&sh->m);
-            break;
-        }
-
-        caesar(sh->buf, sh->buf, (int)n);
-
-        pthread_mutex_lock(&sh->m);
-        sh->len = n;
-        sh->has_data = 1;
-        pthread_cond_signal(&sh->can_consume);
-        pthread_mutex_unlock(&sh->m);
+        queue_push(&data->q, encrypted, bytes);
+        free(encrypted);
     }
-
-    pthread_mutex_lock(&sh->m);
-    pthread_cond_broadcast(&sh->can_consume);
-    pthread_mutex_unlock(&sh->m);
 
     return NULL;
 }
 
-static void* consumer_thread(void* arg) {
-    shared_t* sh = (shared_t*)arg;
+void* consumer(void* arg) { //поток потребителя, который извлекает зашифрованные данные из очереди и записывает их в выходной файл
 
-    while (1) {
-        pthread_mutex_lock(&sh->m);
+    shared_data* data = (shared_data*)arg;
 
-        while (!sh->has_data && !sh->eof && keep_running) {
-            timed_wait_ms(&sh->can_consume, &sh->m, 200);
-        }
+    while (keep_running) {
 
-        if (!keep_running) {
-            pthread_mutex_unlock(&sh->m);
-            break;
-        }
+        int size;
+        unsigned char* chunk = queue_pop(&data->q, &size);
 
-        if (sh->has_data) {
-            size_t n = sh->len;
-
-            unsigned char local[BUF_SIZE];
-            memcpy(local, sh->buf, n);
-
-            sh->has_data = 0;
-            sh->len = 0;
-            pthread_cond_signal(&sh->can_produce);
-            pthread_mutex_unlock(&sh->m);
-
-            size_t wr = fwrite(local, 1, n, sh->out);
-            if (wr != n) {
-                keep_running = 0;
+        if (chunk == NULL) {
+            pthread_mutex_lock(&data->mutex);
+            if (data->finished) {
+                pthread_mutex_unlock(&data->mutex);
                 break;
             }
-        } else if (sh->eof) {
-            pthread_mutex_unlock(&sh->m);
-            break;
-        } else {
-            pthread_mutex_unlock(&sh->m);
+            pthread_mutex_unlock(&data->mutex);
         }
-    }
 
-    pthread_mutex_lock(&sh->m);
-    pthread_cond_broadcast(&sh->can_produce);
-    pthread_mutex_unlock(&sh->m);
+        fwrite(chunk, 1, size, data->output);
+        free(chunk);
+
+        pthread_mutex_lock(&data->mutex);
+        if (data->finished && data->q.count == 0) {
+            pthread_mutex_unlock(&data->mutex);
+            break;
+        }
+        pthread_mutex_unlock(&data->mutex);
+    }
 
     return NULL;
 }
 
-int main(int argc, char** argv) {
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <input_file> <output_file> <key>\n", argv[0]);
-        return 2;
+int main(int argc, char* argv[]) {
+
+    char* input_path = argv[1];
+    char* output_path = argv[2];
+    output_path_to_remove = output_path;
+    char key = argv[3][0];
+
+    FILE* input = fopen(input_path, "rb");
+
+
+    FILE* output = fopen(output_path, "wb");
+    if (!output) {
+        printf("Cannot open output file\n");
+        fclose(input);
+        return 1;
     }
+    //установка ключа для шифрования
+    set_key(key);
+//установка обработчика сигнала для прерывания программы
+    signal(SIGINT, handle_sigint);
 
-    const char* in_path  = argv[1];
-    const char* out_path = argv[2];
-    const char* key_str  = argv[3];
+    shared_data data; //инициализация общей структуры данных для потоков
 
-    if (access(in_path, F_OK) != 0) {
-        fprintf(stderr, "Input file does not exist: %s\n", in_path);
-        return 2;
-    }
+    data.input = input;
+    data.output = output;
+    data.finished = 0;
+    queue_init(&data.q);
+    current_queue = data.q;
 
-    char* endp = NULL;
-    long k = strtol(key_str, &endp, 0);
-    if (endp == key_str || *endp != '\0' || k < -128 || k > 255) {
-        fprintf(stderr, "Invalid key '%s'. Use number in range -128..255.\n", key_str);
-        return 2;
-    }
+    pthread_mutex_init(&data.mutex, NULL);
 
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = on_sigint;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGINT, &sa, NULL) != 0) die("sigaction");
+    pthread_t prod; //создание потоков производителя и потребителя
+    pthread_t cons;
 
-    FILE* in = fopen(in_path, "rb");
-    if (!in) die("fopen input");
+    pthread_create(&prod, NULL, producer, &data); //запуск потока производителя
+    pthread_create(&cons, NULL, consumer, &data);
 
-    FILE* out = fopen(out_path, "wb");
-    if (!out) {
-        fclose(in);
-        die("fopen output");
-    }
-
-    set_key((char)k);
-
-    shared_t sh;
-    memset(&sh, 0, sizeof(sh));
-    sh.in = in;
-    sh.out = out;
-
-    if (pthread_mutex_init(&sh.m, NULL) != 0) die("pthread_mutex_init");
-    if (pthread_cond_init(&sh.can_produce, NULL) != 0) die("pthread_cond_init");
-    if (pthread_cond_init(&sh.can_consume, NULL) != 0) die("pthread_cond_init");
-
-    pthread_t prod, cons;
-
-    if (pthread_create(&prod, NULL, producer_thread, &sh) != 0) die("pthread_create producer");
-    if (pthread_create(&cons, NULL, consumer_thread, &sh) != 0) die("pthread_create consumer");
-
-    pthread_join(prod, NULL);
+    pthread_join(prod, NULL); //ожидание завершения потока производителя
     pthread_join(cons, NULL);
 
-    pthread_cond_destroy(&sh.can_produce);
-    pthread_cond_destroy(&sh.can_consume);
-    pthread_mutex_destroy(&sh.m);
+    fclose(input);
+    fclose(output);
 
-    fclose(in);
-    fclose(out);
+    pthread_mutex_destroy(&data.mutex); //освобождение ресурсов, связанных с общей структурой данных
+    queue_destroy(&data.q);
 
-    if (!keep_running) {
+    if (!keep_running)
         printf("Операция прервана пользователем\n");
-        return 130; 
-    }
 
     return 0;
 }
